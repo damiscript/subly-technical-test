5942215459ebeaca096937d820571c1f9bf06a38
Dami Adesoye <dami98haniel@hotmail.com>
2021-06-30 12:20:16 +0100

    Server Initialization, Installation of Cors and Postgres with basic application set up

---
+++ server/db.js
@@ -0,0 +1,10 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  password: "kth18822",
+  host: "localhost",
+  port: 5432,
+  database: "sublytest"
+});
+module.exports = pool;
